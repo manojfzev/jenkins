b41ce1abc65db911724c1f056049365a54437ac8
root <root@cis-cat-host.us-central1-a.c.gifted-loader-336417.internal>
2022-02-27 13:08:45 +0000

    Added the sudo for the command

--- cis-cat.py
+++ cis-cat.py
@@ -27,11 +27,11 @@ def create_instance(project, zone, name, imagename,subnet):
 def intiatescan(ciscatscaninstnaceip,ciscatscaninstanceuser, ciscatinstancekey, ciscatscanbenchmark):
 
     commands = [
-        "sed -i '/session.2.host=/c\session.2.host='"+ciscatscaninstnaceip+" /root/Assessor-CLI/config/sessions.properties",
-        "sed -i '/session.2.user=/c\session.2.user='"+ciscatscaninstanceuser+" /root/Assessor-CLI/config/sessions.properties",
-        "sed -i '/session.2.identity=/c\session.2.identity='"+ciscatinstancekey+" /root/Assessor-CLI/config/sessions.properties",
-        "cd /root/Assessor-CLI && ./Assessor-CLI.sh -b /root/Assessor-CLI/benchmarks/"+ciscatscanbenchmark+" > output",
-        "grep Total: /root/Assessor-CLI/output"
+        "sudo sed -i '/session.2.host=/c\session.2.host='"+ciscatscaninstnaceip+" /root/Assessor-CLI/config/sessions.properties",
+        "sudo sed -i '/session.2.user=/c\session.2.user='"+ciscatscaninstanceuser+" /root/Assessor-CLI/config/sessions.properties",
+        "sudo sed -i '/session.2.identity=/c\session.2.identity='"+ciscatinstancekey+" /root/Assessor-CLI/config/sessions.properties",
+        "sudo cd /root/Assessor-CLI && ./Assessor-CLI.sh -b /root/Assessor-CLI/benchmarks/"+ciscatscanbenchmark+" > output",
+        "sudo grep Total: /root/Assessor-CLI/output"
     ]
     try:
         finaloutput=''
