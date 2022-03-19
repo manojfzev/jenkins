66ea26e49bd8747c5bc53854511706f7abb795cf
manojfzev <manojfzev@gnal.com>
2022-03-19 15:18:32 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ mkdir /home/$user/.ssh
 chmod 700 /home/$user/.ssh
 touch /home/$user/.ssh/authorized_keys
 chmod 644 /home/$user/.ssh/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9XVqoscJ2iUL01u9G1iCGyESMTnMfF/goMbfNHvVAF89FvAZsjHVqmqMMS8PREI0ZCkxo7ZU2gG8k0BqLoYLtFGPnGuhPlNon+dEULj9sPpTz1AdxyeQ1ZgktBGl+WP/FHVnZ70OPiP7znFQqV5hFh5JfMoaQ6luxJzcHoPpmJK+L/PBf6gMxpgk2GIKq2a/3AnTJqK6QlPzzzG73plLRZ0BtVciOmH6IOhCs7r+FLyoL1WzZUHDZ5yEkPfkpA6/ItXT6GYrTKEcNyTI/i2VQk+cO0uMDLsngIF0VLtrZZEmMyrkDkLHT+tD9wGghzD0Vb8fvGv9xQtiOB7IVCiA1 root@cis-cat-host" >> /home/$user/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDsYmUjfIExHZ4vftoKch1txL3/tTRQSXyXNhrs/ouvJO6YV/rIdijIaCGgEprZSPL+BRY9iMYbVUb2nMaxCeAhx07JdhSeBYuoMwamKxTKTmXF81yqRUMfmhiZrljCCB1llEzwongNr0liodvlKscu0abSbixBir58WZd8eyOcjdsuKH/IIvxmIUW8oBL3d3tU5KpubkUo2mYIvWXTzstA3IVxECKyfKUFc8PrVGeS9C7BDgvRnaF7Ez42wmbWAVcIl2CsSKMMpikOsh84S0BfxrfCiedt1uZ3HYxy15CKWAs8I4/eVl4BkyYRWz2UXRYqWX5gAVwBB+4Tr4HFmC9X root@clops-jenkins-slave-template-1" >> /home/$user/.ssh/authorized_keys
 chown -R $user:$user /home/$user/.ssh
 echo "$user ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
