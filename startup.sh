5008a68e6abc21cff945641bf00defea4f9d05bb
manojfzev <manojfzev@gnal.com>
2022-04-27 11:25:17 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,5 +8,5 @@ chmod 644 /home/$user/.ssh/authorized_keys
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDsYmUjfIExHZ4vftoKch1txL3/tTRQSXyXNhrs/ouvJO6YV/rIdijIaCGgEprZSPL+BRY9iMYbVUb2nMaxCeAhx07JdhSeBYuoMwamKxTKTmXF81yqRUMfmhiZrljCCB1llEzwongNr0liodvlKscu0abSbixBir58WZd8eyOcjdsuKH/IIvxmIUW8oBL3d3tU5KpubkUo2mYIvWXTzstA3IVxECKyfKUFc8PrVGeS9C7BDgvRnaF7Ez42wmbWAVcIl2CsSKMMpikOsh84S0BfxrfCiedt1uZ3HYxy15CKWAs8I4/eVl4BkyYRWz2UXRYqWX5gAVwBB+4Tr4HFmC9X root@clops-jenkins-slave-template-1" >> /home/$user/.ssh/authorized_keys
 chown -R $user:$user /home/$user/.ssh
 echo "$user ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-
+> /opt/Tanium/TaniumClient/Tools/CustomTags
 
